4c9338716ef919d2ad1dd12ac45660683f1a8b22
影月 零 <zero@genshin.org>
2015-04-19 03:06:15 +0900

    wip

--- src/data/collection.cpp
+++ src/data/collection.cpp
@@ -2,42 +2,45 @@
 
 Emojidex::Data::Collection::Collection()
 {
-  moreMethod = NULL;
-  page = 0;
-  limit = 0;
-  total_count = 0;
-  endpoint = "";
+	moreMethod = NULL;
+	page = 0;
+	limit = 0;
+	total_count = 0;
+	endpoint = "";
 }
 
 Emojidex::Data::Collection::~Collection()
 {
 }
 
-Emojidex::Data::Collection Emojidex::Data::Collection::merge(Emojidex::Data::Collection delta_collection)
+Emojidex::Data::Collection Emojidex::Data::Collection::merge(
+		Emojidex::Data::Collection delta_collection)
 {
-  for (auto new_moji : delta_collection.emoji)
-    this->emoji[new_moji.first] = new_moji.second;
+	for (auto new_moji : delta_collection.emoji)
+		this->emoji[new_moji.first] = new_moji.second;
 
-  return *this;
+	return *this;
 }
 
 Emojidex::Data::Collection Emojidex::Data::Collection::operator<<(Emojidex::Data::Collection delta_collection)
 {
-  return this->merge(delta_collection);
+	return this->merge(delta_collection);
 }
 
 Emojidex::Data::Collection Emojidex::Data::Collection::more()
 {
-  if (moreMethod == NULL)
-    return Emojidex::Data::Collection();
+	if (moreMethod == NULL)
+		return Emojidex::Data::Collection();
 
-  Emojidex::Data::Collection collect = this->moreMethod(*this);
+	Emojidex::Data::Collection collect = this->moreMethod(*this);
 
-  this->merge(collect);
+	this->merge(collect);
 
-  return collect;
+	return collect;
 }
 
-void Emojidex::Data::Collection::setPagination(Emojidex::Data::Collection (*moreMethod)(Emojidex::Data::Collection), unsigned int starting_page, unsigned int limit)
+void Emojidex::Data::Collection::setPagination(
+		Emojidex::Data::Collection (*moreMethod)(Emojidex::Data::Collection), 
+		unsigned int starting_page, unsigned int limit)
 {
 }
--- src/data/collection.h
+++ src/data/collection.h
@@ -12,41 +12,53 @@ namespace Emojidex {
 	namespace Data {
 		// emoji base data
 		class Collection
-    {
-    public:
-      Collection();
-      ~Collection();
+		{
+		public:
+			Collection();
+			~Collection();
 
-      unordered_map<string, Emojidex::Data::Emoji> emoji;
+			unordered_map<string, Emojidex::Data::Emoji> emoji;
 
-      // Merge a collection with this collection, overwriting emoji with the same
-      // code in this collection. Rerturns this collection after the merge for chaining.
-      Emojidex::Data::Collection merge(Emojidex::Data::Collection delta_collection);
-      Emojidex::Data::Collection operator<<(Emojidex::Data::Collection delta_collection);
+			// Broken out operations on the emoji map (for interfaces or implementations 
+			// without map functionality)
+			vector<Emojidex::Data::Emoji> all();
+			Emojidex::Data::Emoji findByMoji(string moji);
+			Emojidex::Data::Emoji findByCode(string code);
+			Emojidex::Data::Emoji findByUnicode(string unicode);
+			//Emojidex::Data::Collection search()
+			Emojidex::Data::Collection category(string category);
+
+			// Merge a collection with this collection, overwriting emoji with the same
+			// code in this collection. Rerturns this collection after the merge for chaining.
+			Emojidex::Data::Collection merge(Emojidex::Data::Collection delta_collection);
+			Emojidex::Data::Collection operator<<(Emojidex::Data::Collection delta_collection);
       
-      //===========================================================================================
-      // Service Info
-      // ============
-      string endpoint;
-      bool detailed;
-      string locale;
-      unsigned int page;
-      unsigned short limit;
-      unsigned int total_count;
+			//=====================================================================================
+			// Service Info
+			// ============
+			string endpoint;
+			bool detailed;
+			string locale;
+			unsigned int page;
+			unsigned short limit;
+			unsigned int total_count;
 
-      // Get more of the collection if the collection is paginated and has remaining pages.
-      // Returns true if the next page was sucessfully obtained. Returns false if there are no 
-      // more pages/emoji to obtain.
-      Emojidex::Data::Collection more();
+			// Get more of the collection if the collection is paginated and has remaining pages.
+			// Returns true if the next page was sucessfully obtained. Returns false if there are 
+			// no more pages/emoji to obtain.
+			Emojidex::Data::Collection more();
 
-      // Called when more() is invoked.
-      // Results are combined into this collection but a collection with just the new results is 
-      // returned.
-      Emojidex::Data::Collection (*moreMethod)(Emojidex::Data::Collection);
-      // Sets up collection as a paged collection (with more pages/emoji remaining on the service).
-      void setPagination(Emojidex::Data::Collection (*moreMethod)(Emojidex::Data::Collection), unsigned int starting_page, unsigned int limit);
-    };
-  }
+			// Called when more() is invoked.
+			// Results are combined into this collection but a collection with just the new results 
+			// is returned.
+			Emojidex::Data::Collection (*moreMethod)(Emojidex::Data::Collection);
+			// Sets up collection as a paged collection (with more pages/emoji remaining on the 
+			// service).
+			void setPagination(
+					Emojidex::Data::Collection (*moreMethod)(Emojidex::Data::Collection), 
+					unsigned int starting_page, unsigned int limit);
+		};
+	}
 }
 
 #endif /* EMOJIDEX_DATA_COLLECTION_H */
