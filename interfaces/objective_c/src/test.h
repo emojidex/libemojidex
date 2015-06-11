8ab1da95da64bb0f4b8a27c855d85a1121b6189a
kou <kou@genshin.org>
2015-06-11 14:53:10 +0900

    Create objective-c library test.

---
+++ interfaces/objective_c/src/test.h
@@ -0,0 +1,6 @@
+
+#import <Foundation/Foundation.h>
+
+@interface Hoge : NSObject
+- (void)foo;
+@end
---
+++ interfaces/objective_c/src/test.mm
@@ -0,0 +1,10 @@
+
+#import "test.h"
+#import <Foundation/NSObject.h>
+
+@implementation Hoge
+- (void)foo
+{
+  NSLog(@"hoge\n");
+}
+@end
