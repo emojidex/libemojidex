4f48b614a4f8ba41d0036f2eaa7fc7129b58f6a6
影月 零 <zero@genshin.org>
2015-08-23 17:36:53 +0900

    add postbuild

---
+++ interfaces/android/postbuild.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'git'
+require 'fileutils'
+
+@build_dir = ARGV[0] || Dir.pwd
+@source_dir = ARGV[1] || "#{@build_dir}/../"
+@interface_dir = "#{@build_dir}/com/emojidex/"
+
+FileUtils.mkdir_p(@interface_dir)
+
+puts "== Creating Native Interface sources in #{@build_dir}"
+
+`swig -c++ -java -package com.emojidex -outdir #{@interface_dir} -o #{source_dir}/src/emojidex.h #{source_dir}/src/emojidex.i`
