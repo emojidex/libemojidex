5e1497b7bf6025eb37805690e91a46f787fe17af
影月 零 <zero@genshin.org>
2015-09-14 20:21:49 +0900

    wip

--- interfaces/android/jni/Android.mk
+++ interfaces/android/jni/Android.mk
@@ -5,24 +5,19 @@ OR_INCLUDE_PATH := LOCAL_PATH
 OR_CPP_SOURCES :=
 
 include $(CLEAR_VARS)
-#TARGET_ARCH 	:= arm
-#TARGET_ARCH_ABI := armeabi
-#TARGET_PLATFORM := android-21
-#TARGET_ABI		:= android-21-armeabi
 
 LOCAL_MODULE    := libemojidex-android
 LOCAL_SRC_FILES := libemojidex_wrap.cpp $(OR_CPP_SOURCES)
-#LOCAL_CPP_FEATURES += exceptions rtti pthread
+#LOCAL_CPP_FEATURES += exceptions rtti
 #LOCAL_CFLAGS    := -frtti -fexceptions
 LOCAL_C_INCLUDES += $(OR_INCLUDE_PATH)
 
 #$(call import-add-path, $(OR_LIB_PATH))
-#LOCAL_STATIC_LIBRARIES 	:= boost_system-gcc-mt-1_53 boost_filesystem-gcc-mt-1_53 ssl crypto
 
-#LOCAL_LDLIBS 	:= -L$(OR_LIB_PATH) -lboost_system -lboost_filesystem -lz -lssl -lcrypto
-LOCAL_STATIC_LIBRARIES 	:= boost_system_static boost_filesystem_static z_static ssl_static crypto_static
-#LOCAL_STATIC_LIBRARIES := emojidex_static
+#LOCAL_PATH += $(OR_LIB_PATH)/$(TARGET_ARCH)
+LOCAL_LDLIBS 	+= -L$(OR_LIB_PATH)/$(TARGET_ARCH) -lssl -lcrypto
+LOCAL_DYNAMIC_LIBRARIES += boost_system boost_filesystem z crypto ssl
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,boost/1.57.0)
+$(call import-module,boost/1.58.0)
--- interfaces/android/postbuild.rb
+++ interfaces/android/postbuild.rb
@@ -19,8 +19,8 @@ FileUtils.cp_r("#{@source_dir}/interfaces/android/jni", @build_dir, {remove_dest
 txt = File.read("#{@build_dir}/jni/Android.mk")
 
 # set source path
-txt.gsub!("$(OR_INCLUDE_PATH)", "#{@source_dir}/src/ #{@source_dir}/vendor/rapidjson/include/ #{@natives_dir}/natives/arm/include/")
-txt.gsub!("$(OR_LIB_PATH)", "#{@natives_dir}/natives/arm/lib/")
+txt.gsub!("$(OR_INCLUDE_PATH)", "#{@source_dir}/src/ #{@source_dir}/vendor/rapidjson/include/ #{@natives_dir}/natives/include/ #{ENV["CRYSTAX_NDK"]}/sources/boost/1.58.0/include/")
+txt.gsub!("$(OR_LIB_PATH)", "#{@natives_dir}/natives/lib/")
 
 cpp_files = Dir.glob("#{@source_dir}/src/**/*.cpp")
 cpp_files_string = ""
@@ -30,6 +30,7 @@ txt.gsub!("$(OR_CPP_SOURCES)", cpp_files_string )
 File.open("#{@build_dir}/jni/Android.mk", "w") { |mkfile| mkfile.puts txt }
 
 puts "== Creating Native Interface Java sources"
+puts "swig -c++ -java -package com.emojidex.libemojidex -outdir #{@interface_dir} -o #{@build_dir}/jni/libemojidex_wrap.cpp #{@source_dir}/src/emojidex.i"
 `swig -c++ -java -package com.emojidex.libemojidex -outdir #{@interface_dir} -o #{@build_dir}/jni/libemojidex_wrap.cpp #{@source_dir}/src/emojidex.i`
 
 puts "== Building JNI bindings"
