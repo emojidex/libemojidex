1341906f53420f46f19f3d0fa8c39878903efa7a
影月 零 <zero@genshin.org>
2015-11-17 03:51:18 +0900

    re-tune preparation script

--- interfaces/android/prepare.rb
+++ interfaces/android/prepare.rb
@@ -4,6 +4,8 @@ require 'git'
 require 'fileutils'
 
 @build_dir = ARGV[0] || Dir.pwd
+Dir.chdir(@build_dir)
+@build_dir = Dir.pwd
 
 @build_api_level = 14
 @build_targets = ["arm", "arm64", "x86", "x86_64"] #, "mips"]
@@ -61,9 +63,9 @@ def setup_paths()
   # jumper links
   @build_targets.each do |target|
     Dir.mkdir("#{@build_dir}/natives/#{target}") unless File.exists? "#{@build_dir}/natives/#{target}"
-    FileUtils.ln_s("#{@build_dir}/natives/bin/#{target}", "#{@build_dir}/natives/#{target}/bin")
-    FileUtils.ln_s("#{@build_dir}/natives/include/#{target}", "#{@build_dir}/natives/#{target}/include")
-    FileUtils.ln_s("#{@build_dir}/natives/lib/#{target}", "#{@build_dir}/natives/#{target}/lib")
+    FileUtils.ln_s("#{@build_dir}/natives/bin/#{target}", "#{@build_dir}/natives/#{target}/bin") unless File.exists? "#{@build_dir}/natives/#{target}/bin"
+    FileUtils.ln_s("#{@build_dir}/natives/include/#{target}", "#{@build_dir}/natives/#{target}/include") unless File.exists? "#{@build_dir}/natives/#{target}/include"
+    FileUtils.ln_s("#{@build_dir}/natives/lib/#{target}", "#{@build_dir}/natives/#{target}/lib") unless File.exists? "#{@build_dir}/natives/#{target}/lib"
   end
 end
 
@@ -92,16 +94,24 @@ end
 def build_OpenSSL()
   if Dir.exists? "#{@build_dir}/openssl"
     puts "OpenSSL repository found. Updating..."
-    git = Git.open("#{@build_dir}/openssl")
-    git.reset_hard("HEAD")
-    git.clean({force: true, d: true, x:true})
-    git.checkout("OpenSSL_1_0_2-stable")
-    git.pull
+    #git = Git.open("#{@build_dir}/openssl")
+    Dir.chdir "#{@build_dir}/openssl"
+    #git.reset_hard('HEAD')
+    `git reset HEAD --hard`
+    #git.clean({force: true, d: true, x:true})
+    `git clean -fdx`
+    #git.checkout("OpenSSL_1_0_2-stable")
+    `git checkout OpenSSL_1_0_2-stable`
+    #git.pull
+    `git pull`
     puts 'Updated.'
   else
     puts 'OpenSSL repository not found. Cloning...'
-    git = Git.clone("https://github.com/openssl/openssl.git", "#{@build_dir}/openssl")
-    git.checkout("OpenSSL_1_0_2-stable")
+    Dir.chdir "#{@build_dir}"
+    #git = Git.clone('https://github.com/openssl/openssl.git', "#{@build_dir}/openssl")
+    `git clone https://github.com/openssl/openssl.git openssl`
+    #git.checkout("OpenSSL_1_0_2-stable")
+    `git checkout OpenSSL_1_0_2-stable`
     puts 'Cloned.'
   end
 
@@ -109,47 +119,52 @@ def build_OpenSSL()
   Dir.chdir "#{@build_dir}/openssl"
   
   puts '= Building for arm'
-  `make clean && make dclean`
-  git.clean({force: true, d: true, x:true})
-  `CC="#{@build_dir}/toolchains/arm/bin/arm-linux-androideabi-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-arm" ./Configure android-armv7 shared threads no-asm && make`
-  FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/arm")
-  FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/arm")
-  FileUtils.ln_s("#{@build_dir}/openssl/include/openssl", "#{@build_dir}/natives/include/openssl", { force: true })
+  #`make clean && make dclean`
+  #git.clean({force: true, d: true, x:true})
+  `git clean -fdx`
+  `CC="#{@build_dir}/toolchains/arm/bin/arm-linux-androideabi-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-arm" ./Configure --prefix=#{@build_dir}/natives/arm/ android-armv7 shared threads no-asm && make && make install`
+  #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/arm")
+  #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/arm")
+  #FileUtils.ln_s("#{@build_dir}/openssl/include/openssl", "#{@build_dir}/natives/include/openssl", { force: true })
 
   puts '= Building for x86'
-  `make clean && make dclean`
-  git.clean({force: true, d: true, x:true})
-  `CC="#{@build_dir}/toolchains/x86/bin/i686-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-x86" ./Configure android-x86 shared threads no-asm && make`
-  FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/x86")
-  FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/x86")
+  #`make clean && make dclean`
+  #git.clean({force: true, d: true, x:true})
+  `git clean -fdx`
+  `CC="#{@build_dir}/toolchains/x86/bin/i686-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-x86" ./Configure --prefix=#{@build_dir}/natives/x86/ android-x86 shared threads no-asm && make && make install`
+  #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/x86")
+  #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/x86")
 
 
   if @build_targets.include? 'mips'
     puts '= Building for mips'
-    `make clean && make dclean`
-    git.clean({force: true, d: true, x:true})
-    `CC="#{@build_dir}/toolchains/mips/bin/mipsel-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-mips" ./Configure android-mips shared threads no-asm && make`
-    FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/mips")
-    FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/mips")
+    #`make clean && make dclean`
+    #git.clean({force: true, d: true, x:true})
+    `git clean -fdx`
+    `CC="#{@build_dir}/toolchains/mips/bin/mipsel-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-mips" ./Configure --prefix=#{@build_dir}/natives/mips/ android-mips shared threads no-asm && make && make install`
+    #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/mips")
+    #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/mips")
   end
 
   if @build_api_level > 20
     if @build_targets.include? 'arm64'
       puts '= Building for arm64'
-      `make clean && make dclean`
-      git.clean({force: true, d: true, x:true})
-      `CC="#{@build_dir}/toolchains/arm64/bin/aarch64-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-arm64" ./Configure android shared threads no-asm && make`
-      FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/arm64")
-      FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/arm64")
+      #`make clean && make dclean`
+      #git.clean({force: true, d: true, x:true})
+      `git clean -fdx`
+      `CC="#{@build_dir}/toolchains/arm64/bin/aarch64-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-arm64" ./Configure --prefix=#{@build_dir}/natives/arm64/ android shared threads no-asm && make && make install`
+      #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/arm64")
+      #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/arm64")
     end
 
     if @build_targets.include? 'x86_64'
       puts '= Building for x86_64'
-      `make clean && make dclean`
-      git.clean({force: true, d: true, x:true})
-      `CC="#{@build_dir}/toolchains/x86_64/bin/x86_64-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-x86_64" ./Configure android-x86 shared threads no-asm && make`
-      FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/x86_64")
-      FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/x86_64")
+      #`make clean && make dclean`
+      #git.clean({force: true, d: true, x:true})
+      `git clean -fdx`
+      `CC="#{@build_dir}/toolchains/x86_64/bin/x86_64-linux-android-gcc --sysroot=$CRYSTAX_NDK/platforms/android-#{@build_api_level}/arch-x86_64" ./Configure --prefix=#{@build_dir}/natives/x86_64/ android-x86 shared threads no-asm && make && make install`
+      #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libcrypto.so*"), "#{@build_dir}/natives/lib/x86_64")
+      #FileUtils.cp(Dir.glob("#{@build_dir}/openssl/libssl.so*"), "#{@build_dir}/natives/lib/x86_64")
     end
   end
 
@@ -177,104 +192,36 @@ def build_curl()
   Dir.chdir "#{@build_dir}/curl"
   
   puts '= Building for arm'
-  `make clean && make dclean`
-  git.clean({force: true, d: true, x:true})
-  #puts "#{full_env_override('arm')} ./buildconf && ./configure --host=arm-linux-androideabi --with-openssl-includes=#{@build_dir}/natives/include/arm --with-openssl-libraries=#{@build_dir}/natives/lib/arm --prefix=#{@build_dir}/natives/arm #{disable_lines} && make && make install"
+  `git clean -fdx`
   `#{chain_env('arm')} LDFLAGS=\"-L#{@build_dir}/natives/lib/arm -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/arm \" ./buildconf`
-  `#{chain_env('arm')} LDFLAGS=\"-L#{@build_dir}/natives/lib/arm -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/arm \" ./configure --host=arm-linux-androideabi --with-openssl-includes=#{@build_dir}/natives/include/arm --with-openssl-libraries=#{@build_dir}/natives/lib/arm --with-ssl --prefix=#{@build_dir}/natives/arm #{disable_lines} && make && make install`
+  `#{chain_env('arm')} LDFLAGS=\"-L#{@build_dir}/natives/lib/arm -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/arm \" ./configure --host=arm-linux-androideabi --with-openssl-includes=#{@build_dir}/natives/include/arm --with-openssl-libraries=#{@build_dir}/natives/lib/arm --with-ssl --prefix=#{@build_dir}/natives/arm/ #{disable_lines} && make && make install`
 
- # puts '= Building for x86'
- # `make clean && make dclean`
- # git.clean({force: true, d: true, x:true})
- # `#{full_env_override('x86')} ./buildconf && ./configure --prefix=#{@build_dir}/natives/x86 && make && make install`
+  puts '= Building for x86'
+  `git clean -fdx`
+  `#{chain_env('x86')} LDFLAGS=\"-L#{@build_dir}/natives/lib/x86 -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/x86 \" ./buildconf`
+  `#{chain_env('x86')} LDFLAGS=\"-L#{@build_dir}/natives/lib/x86 -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/x86 \" ./configure --with-openssl-includes=#{@build_dir}/natives/include/x86 --with-openssl-libraries=#{@build_dir}/natives/lib/x86 --with-ssl --prefix=#{@build_dir}/natives/x86/ #{disable_lines} && make && make install`
 
- # if @build_targets.include? 'mips'
- #   puts '= Building for mips'
- #   `make clean && make dclean`
- #   git.clean({force: true, d: true, x:true})
- #   `#{full_env_override('mips')} ./buildconf && ./configure --prefix=#{@build_dir}/natives/mips && make`
- # end
+#  if @build_targets.include? 'mips'
+#    puts '= Building for mips'
+#    `git clean -fdx`
+#    `#{full_env_override('mips')} ./buildconf && ./configure --prefix=#{@build_dir}/natives/mips && make`
+#  end
 
- # if @build_api_level > 20
- #   if @build_targets.include? 'arm64'
- #     puts '= Building for arm64'
- #     `make clean && make dclean`
- #     git.clean({force: true, d: true, x:true})
- #     `#{full_env_override('arm64')} ./buildconf && ./configure --prefix=#{@build_dir}/natives/arm64 && make`
- #   end
- # 
- #   if @build_targets.include? 'x86_64'
- #     puts '= Building for x86_64'
- #     `make clean && make dclean`
- #     git.clean({force: true, d: true, x:true})
- #     `#{full_env_override('x86_64')} ./buildconf && ./configure --prefix=#{@build_dir}/natives/x86_64 && make`
- #   end
- # end
-end
-
-# Boost for Android
-def build_android_boost()
-  if Dir.exists? "#{@build_dir}/Boost-for-Android"
-    puts 'Boost for Android repository found. Updating...'
-    git = Git.open("#{@build_dir}/Boost-for-Android")
-    git.clean({force: true, d: true, x:true})
-    git.pull
-    puts 'Updated.'
-  else
-    puts 'Boost for Android directory not found. Cloning...'
-    git = Git.clone("https://github.com/MysticTreeGames/Boost-for-Android.git", "#{@build_dir}/Boost-for-Android")
-    puts 'Cloned.'
+  if @build_api_level > 20
+    if @build_targets.include? 'arm64'
+      puts '= Building for arm64'
+      `git clean -fdx`
+      `#{chain_env('arm64')} LDFLAGS=\"-L#{@build_dir}/natives/lib/arm64 -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/arm64 \" ./buildconf`
+      `#{chain_env('arm64')} LDFLAGS=\"-L#{@build_dir}/natives/lib/arm64 -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/arm64 \" ./configure --host=arm-linux-androideabi --with-openssl-includes=#{@build_dir}/natives/include/arm64 --with-openssl-libraries=#{@build_dir}/natives/lib/arm64 --with-ssl --prefix=#{@build_dir}/natives/arm64/ #{disable_lines} && make && make install`
+    end
+  
+    if @build_targets.include? 'x86_64'
+      puts '= Building for x86_64'
+      `git clean -fdx`
+      `#{chain_env('x86_64')} LDFLAGS=\"-L#{@build_dir}/natives/lib/x86_64 -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/x86_64 \" ./buildconf`
+      `#{chain_env('x86_64')} LDFLAGS=\"-L#{@build_dir}/natives/lib/x86_64 -lssl -lcrypto\" CFLAGS=\"-I#{@build_dir}/natives/include/x86_64 \" ./configure --with-openssl-includes=#{@build_dir}/natives/include/x86_64 --with-openssl-libraries=#{@build_dir}/natives/lib/x86_64 --with-ssl --prefix=#{@build_dir}/natives/x86_64/ #{disable_lines} && make && make install`
+    end
   end
-
-  puts '== Building Boost for Android'
-  Dir.chdir "#{@build_dir}/Boost-for-Android"
-  `./build-android.sh #{ENV['CRYSTAX_NDK']}`
-  if $?.exitstatus == 0
-    puts 'Build appears to have succeeded. Continuing.'
-    FileUtils.cp_r("#{@build_dir}/Boost-for-Android/build/include/", "#{@build_dir}/natives/arm/")
-    FileUtils.cp_r("#{@build_dir}/Boost-for-Android/build/lib/", "#{@build_dir}/natives/arm/")
-    FileUtils.ln_s("#{@build_dir}/natives/arm/include/boost-1_53/boost", "#{@build_dir}/natives/arm/include/boost", { force: true })
-  else
-    exit 2
-  end
-  Dir.chdir @build_dir
-end
-
-def build_boost()
-  Dir.chdir "#{@build_dir}"
-  if Dir.exists? "#{@build_dir}/boost"
-    puts "Boost repository found. Moving to #{@build_dir}/boost"
-    puts 'Updating...'
-    Dir.chdir "#{@build_dir}/boost"
-    `git pull`
-    `git submodule update --recursive`
-    puts 'Updated.'
-  else
-    puts 'Boost directory not found. Cloning...'
-    git = Git.clone("https://github.com/boostorg/boost.git", "#{@build_dir}/boost")
-    puts "Cloned. Moving to #{@build_dir}/boost"
-    puts 'Obtaining submodules (this will take a long time)...'
-    Dir.chdir "#{@build_dir}/boost"
-    `git submodule init`
-    `git submodule update --recursive`
-  end
-
-  puts 'Building Boost'
-  Dir.chdir "#{@build_dir}/boost"
-  puts "Moved to #{Dir.getwd}. Running build."
-  `CC="#{@build_dir}/toolchains/arm/bin/arm-linux-androideabi-gcc --sysroot=$CRYSTAX_NDK/platforms/android-19/arch-arm" ./bootstrap.sh --without-libraries=python`
-  `CC="#{@build_dir}/toolchains/arm/bin/arm-linux-androideabi-gcc --sysroot=$CRYSTAX_NDK/platforms/android-19/arch-arm" ./b2 headers`
-  `CC="#{@build_dir}/toolchains/arm/bin/arm-linux-androideabi-gcc --sysroot=$CRYSTAX_NDK/platforms/android-19/arch-arm" ./b2`
-  `CC="#{@build_dir}/toolchains/arm/bin/arm-linux-androideabi-gcc --sysroot=$CRYSTAX_NDK/platforms/android-19/arch-arm" ./b2 install --prefix=#{@build_dir}/natives/arm/`
-  if $?.exitstatus == 0
-    puts 'Build appears to have succeeded. Continuing.'
-    #FileUtils.cp_r("#{@build_dir}/boost/build/include/", "#{@build_dir}/natives/arm/")
-    #FileUtils.cp_r("#{@build_dir}/Boost-for-Android/build/lib/", "#{@build_dir}/natives/arm/")
-    #FileUtils.ln_s("#{@build_dir}/natives/arm/include/boost-1_53/boost", "#{@build_dir}/natives/arm/include/boost", { force: true })
-  else
-    exit 2
-  end
-  Dir.chdir @build_dir
 end
 
 def check_lock()
@@ -308,9 +255,7 @@ check_env()
 setup_paths()
 prepare_chains()
 build_OpenSSL()
-#build_boost()
 build_curl()
-#build_android_boost()
 set_lock()
 
 exit 0
