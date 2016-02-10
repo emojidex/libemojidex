003c71fac11b9439a264ab8f8c666e38d3b03cdd
kou <kou@genshin.org>
2016-02-10 16:53:13 +0900

    Remove trush.

--- spec/libemojidex_spec.cpp
+++ spec/libemojidex_spec.cpp
@@ -362,10 +362,6 @@ BOOST_AUTO_TEST_SUITE(service_search_suite)
 	//BOOST_AUTO_TEST_CASE(term) {
 		//BOOST_CHECK_GT(search.term("tears").size(), 0);
 	//}
-
-	BOOST_AUTO_TEST_CASE(find) {
-		search.find(":ガソリンスタンド:");
-	}
 BOOST_AUTO_TEST_SUITE_END()
 
 ///////////////////////////////////////////////////////////////////////////////
