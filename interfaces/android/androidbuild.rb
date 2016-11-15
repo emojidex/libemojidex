5d7292042b56d940921fb78bd7f67284b7d0be1c
kou <kou@genshin.org>
2016-11-15 14:40:10 +0900

    Copy so only.

--- interfaces/android/androidbuild.rb
+++ interfaces/android/androidbuild.rb
@@ -34,11 +34,11 @@ puts "== copying natives"
 FileUtils.mkdir_p(@interface_dir)
 FileUtils.mkdir_p("#{@build_dir}/libs")
 FileUtils.mkdir_p("#{@build_dir}/libs/armeabi")
-FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/arm/*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/armeabi/", {remove_destination: true})
+FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/arm/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/armeabi/", {remove_destination: true})
 FileUtils.mkdir_p("#{@build_dir}/libs/x86")
-FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/x86/*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/x86/", {remove_destination: true})
+FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/x86/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/x86/", {remove_destination: true})
 FileUtils.mkdir_p("#{@build_dir}/libs/mips")
-FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/mips/*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/mips/", {remove_destination: true})
+FileUtils.cp_r(Dir["#{@natives_dir}/natives/lib/mips/*.so*"].collect{|f| File.expand_path(f)}, "#{@build_dir}/libs/mips/", {remove_destination: true})
 
 puts "== for mac (copy the missing file)"
 FileUtils.cp("#{@natives_dir}/natives/lib/arm/libcurl.so", "#{@build_dir}/libs/armeabi/libcurl.so") unless File.exists? "#{@build_dir}/libs/armeabi/libcurl.so"
